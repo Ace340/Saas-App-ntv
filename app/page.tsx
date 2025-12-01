2a8f3a34181c2991247836230e84dad74a0c9008
Ace340 <53065538+Ace340@users.noreply.github.com>
2025-12-01 14:08:25 -0500

    Added: Hompage

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,52 @@
+import CompanionCard from '@/components/CompanionCard'
+import CompanionList from '@/components/CompanionList'
+import CTA from '@/components/CTA'
 import { Button } from '@/components/ui/button'
+import { recentSessions } from '@/constants'
 import React from 'react'
 
 const Page = () => {
   return (
-    <div>
-      <h1 className='text-2xl underline'>Welcome to my Saas App</h1>
-      <Button>
-        Get Started
-      </Button>
-    </div>
+    <main>
+        <h1 className='text-2xl underline'>Popular Companions</h1>
+
+        <section className='home-section'>
+          <CompanionCard
+            id="123"
+            name="Neura The Brainy Explorer"
+            topic="Neuroscience"
+            subject="science"
+            duration={45}
+            color="#ffda6e"
+          />
+          <CompanionCard
+            id="456"
+            name="Neura The Brainy Explorer"
+            topic="Neuroscience"
+            subject="science"
+            duration={45}
+            color="#ffda6e"
+          />
+          <CompanionCard
+            id="789"
+            name="Neura The Brainy Explorer"
+            topic="Neuroscience"
+            subject="science"
+            duration={45}
+            color="#ffda6e"
+          />
+        </section>
+
+        <section className='home-section'>
+          <CompanionList 
+            title="Your Recent Sessions"
+            companions={recentSessions}
+            classNames="w-2/3 max-lg:w-full"
+          />
+          <CTA />
+        </section>
+    </main>
+    
   )
 }
 
